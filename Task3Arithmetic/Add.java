310a2675a045d87b78d326e050e9698907a14281
JBorr <jovetborrico@gmail.com>
2024-09-10 10:52:22 +0000

    update

---
+++ Task3Arithmetic/Add.java
@@ -0,0 +1,12 @@
+public class Add{
+public static void main(String[] args) {
+   int A = 10;
+   int B = 5;
+   int Sum = A+B; 
+   int Difference = A-B;
+
+System.out.println("Sum: "+ Sum);
+System.out.println("Difference: "+ Difference);
+
+}
+}
