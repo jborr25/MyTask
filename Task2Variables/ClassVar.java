2ffd30706a314774ffb17065b1e5efab8c860e7c
JBorr <jovetborrico@gmail.com>
2024-09-10 06:49:56 +0000

    update

--- Task2Variables/ClassVar.java
+++ Task2Variables/ClassVar.java
@@ -1,4 +1,6 @@
 package Task2Variables;
+//are variables that belong to the class itself rather than to individual objects of the class. 
+//This means that there's only one copy of a class variable shared by all instances of the class.
 
 public class ClassVar {
     static String Family = "BORRICO"; //this is a class variable(static fields)
@@ -15,9 +17,13 @@ public class ClassVar {
         ClassVar M2 = new ClassVar("Fiel ", 8);
         ClassVar M3 = new ClassVar("Pinky ", 35);
 
+//Family and Borrico are a class variable declared with the static keyword.
+//the class Family remains the same. The M1, M2, M3 objects share the same (Family) value.
+//Every time a new ClassVar object is created, the Borrico variable is incremented.
+
 System.out.println(M1.Name + ClassVar.Family +" "+ M1.Age);
 System.out.println(M2.Name + ClassVar.Family +" "+ M2.Age);
 System.out.println(M3.Name + ClassVar.Family +" "+ M3.Age);
-System.out.println("Members: " + ClassVar.Borrico);
+System.out.println("Members: " + ClassVar.Borrico); 
     }
 }
--- Task2Variables/InstanceVar.java
+++ Task2Variables/InstanceVar.java
@@ -1,12 +1,22 @@
 package Task2Variables;
 
 public class InstanceVar {
+
     String Name; //instance variable or non-static fields
     int Age; //instance variable or non-static fields
-}
 
-//Instance variables are declared inside a class but outside any method.
-//They are unique to each class instance, meaning each object created from the class has its own copy of these variables.
-//Instance variables are variables that belong to an individual object of a class. 
-//Each object created from a class has its own copy of instance variables. 
-//This allows objects to maintain their own state and behavior.
+    public static void main(String[] args) {
+    
+    InstanceVar Name1 = new InstanceVar();
+    InstanceVar Name2 = new InstanceVar();
+
+    Name1.Name = "Jovz";
+    Name1.Age = 34;
+
+    Name2.Name = "Pinky";
+    Name2.Age = 35;
+
+    System.out.println("Name: " + Name1.Name + " Age: " + Name1.Age);
+    System.out.println("Name: " + Name2.Name + " Age: " + Name2.Age);
+    }
+}
