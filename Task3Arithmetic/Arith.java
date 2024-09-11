6d5858b006ff039667ab12f02fb68051af29783f
JBorr <jovetborrico@gmail.com>
2024-09-11 22:04:33 +0000

    update

--- Task3Arithmetic/Arith.java
+++ Task3Arithmetic/Arith.java
@@ -2,11 +2,26 @@ public class Arith{
 public static void main(String[] args) {
    int A = 10;
    int B = 5;
+   int C = 0;
+   int D = 22;
+   int E = 5;
+   int F = 10;
+
    int Sum = A+B; 
    int Difference = A-B;
+   int Product = A*B;
+   int Qoutient = A/B;
+   int Remainder = D%E; //The modulus operator returns the remainder when the first operand is divided by the second.
+   ++C; //The increment operator increases the value of an operand by 1. It can be used as a prefix (++a) or a suffix (a++).
+   --F;
 
 System.out.println("Sum: "+ Sum);
 System.out.println("Difference: "+ Difference);
+System.out.println("Product: "+ Product);
+System.out.println("Qoutient: " + Qoutient);
+System.out.println("Remainder: " + Remainder);
+System.out.println("Prefix Increment: " + C);
+System.out.println("Prefix Decrement: " + F);
 
     }
 }
