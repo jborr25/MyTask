71913e8a2bbeb7f698f20a75d0b6d1f42b532767
JBorr <jovetborrico@gmail.com>
2024-09-22 11:20:43 +0000

    update

---
+++ Task6-Statements/IfSample.java
@@ -0,0 +1,12 @@
+
+
+public class IfSample {
+    public static void main(String[] args) {
+        int x =  5;
+        if (x > 1) {
+
+        System.out.println("Positive");
+            
+        }
+    }
+}
