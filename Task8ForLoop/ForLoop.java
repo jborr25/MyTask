0483d520b687065b49c4766eb437500a2f0ed116
JBorr <jovetborrico@gmail.com>
2024-09-28 21:17:53 +0000

    update

---
+++ Task8ForLoop/ForLoop.java
@@ -0,0 +1,12 @@
+package Task8ForLoop;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        
+        for (int x=0; x<=20; x++) {
+            System.out.println(x);
+
+        }
+    }
+    
+}
