8ab16943e8ab330cddebcf544805a61e0d018ab8
JBorr <jovetborrico@gmail.com>
2024-09-30 11:28:27 +0000

    update

---
+++ Task11Array/ArrayLesson.java
@@ -0,0 +1,34 @@
+package Task11Array;
+
+public class ArrayLesson {
+    //Arrays in Java are powerful data structures that store a collection of elements of the same data type.
+/**
+ * Key Characteristics
+ * Fixed Size: Once declared, the size of an array cannot be changed.
+ * Homogeneous Elements: All elements in an array must be of the same data type.
+ * Indexing: Elements are accessed using their index, which starts from 0.
+ * Memory Allocation: Arrays are allocated in contiguous memory locations.
+ */
+
+
+
+/**
+ * Declaration: dataType[] arrayName;
+ * Direct Initialization: int[] numbers = {1, 2, 3, 4, 5};
+ * 
+ * Indirect initialization: int[] numbers = new int[5];
+ * numbers[0] = 1;
+ * numbers[1] = 2;
+ * 
+ * Accessing Elements: int firstElement = numbers[0];
+ * Iterating Over Arrays: for (int i = 0; i < numbers.length; i++) {System.out.println(numbers[i]);}
+ * 
+ * Enhanced for Loop: for (int number : numbers) {System.out.println(number);}
+ * Array Length: int arrayLength = numbers.length;
+ * Multidimensional Arrays: int[][] matrix = new int[3][4];
+ * 
+
+ */
+
+
+}
