4f02bae95b7a22d038b8c8ffd725461339ee30bd
JBorr <jovetborrico@gmail.com>
2024-09-30 16:07:28 +0000

    up

---
+++ Task11Array/ArrayList1.java
@@ -0,0 +1,15 @@
+package Task11Array;
+import java.util.*;
+public class ArrayList1 {
+    public static void main(String[] args) {
+        List colors = new ArrayList<>();
+        colors.add("red");
+        colors.add("green");
+        colors.add(23);
+        System.out.println(colors.add("blue"));
+        System.out.println(colors);
+        // colors.add(input.nextLine());
+
+
+    }
+}
